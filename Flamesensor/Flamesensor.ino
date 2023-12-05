a4b44641c230cd78f205753b281c2b3103080911
Mann Patel <115395169+Mann6805@users.noreply.github.com>
2023-12-05 22:15:19 +0530

    Update Flamesensor.ino

--- Flamesensor/Flamesensor.ino
+++ Flamesensor/Flamesensor.ino
@@ -1,14 +1,23 @@
+// Define the pin for the flame sensor
 const int flamesensor = 2;
 
 void setup()
 {
+  // Start serial communication at 9600 bits per second
   Serial.begin(9600);
+
+  // Set the flame sensor pin as an input
   pinMode(flamesensor, INPUT);
 }
 
 void loop()
 {
+  // Read the digital value from the flame sensor pin
   int data = digitalRead(flamesensor);
+
+  // Print the read value to the Serial Monitor
   Serial.println(data);
+
+  // Delay for 1 second before the next iteration
   delay(1000);  
 }

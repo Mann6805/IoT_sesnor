d19b6deed1bafe6207a77c334cc77943f00db54b
Mann Patel <115395169+Mann6805@users.noreply.github.com>
2023-12-05 22:16:34 +0530

    Update airSensor.ino

--- airSensor/airSensor.ino
+++ airSensor/airSensor.ino
@@ -1,16 +1,18 @@
-const int airSensor = 2;
+const int airSensor = 2; // Define the pin where the air sensor is connected
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(airSensor,INPUT);  
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  pinMode(airSensor, INPUT); // Set airSensor pin as input
 }
 
-void loop()
-{
-  int data = digitalWrite(airSensor);
-  Serial.println(data);
-  int per = map(data, 0, 1023.0,100);
-  Serial.println(per);
-  delay(1000);
+void loop() {
+  int sensorValue = analogRead(airSensor); // Read analog data from the air sensor
+  Serial.print("Raw Sensor Value: "); 
+  Serial.println(sensorValue); // Print the raw sensor value to the serial monitor
+  
+  int percentage = map(sensorValue, 0, 1023, 0, 100); // Map the sensor value to a percentage (0-100)
+  Serial.print("Percentage: ");
+  Serial.println(percentage); // Print the percentage to the serial monitor
+  
+  delay(1000); // Wait for 1 second before taking the next reading
 }

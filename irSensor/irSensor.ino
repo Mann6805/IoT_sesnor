e6d920b802532ef33df50a81b81c1bc3ad808cc5
Mann Patel <115395169+Mann6805@users.noreply.github.com>
2023-12-05 22:19:22 +0530

    Update irSensor.ino

--- irSensor/irSensor.ino
+++ irSensor/irSensor.ino
@@ -1,25 +1,25 @@
-const int irSensor = 2;
-const int led = 15;
+const int irSensor = 2; // Pin connected to the IR sensor
+const int led = 15; // Pin connected to the LED
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(irSensor, INPUT);
-  pinMode(led,OUTPUT);
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  pinMode(irSensor, INPUT); // Set IR sensor pin as input
+  pinMode(led, OUTPUT); // Set LED pin as output
 }
 
-void loop()
-{
-  int data = digitalRead(irSensor); 
-  if(data==0){
-      digitalWrite(led,HIGH);
-      char turu[]="Object detected";
-      Serial.println(turu);
-    }
-  else{
-     digitalWrite(led,LOW);
-     char furu[]="Object not detected";
-     Serial.println(furu);
-      }
-  delay(100);
+void loop() {
+  int data = digitalRead(irSensor); // Read data from IR sensor
+
+  // Check if an object is detected by the IR sensor
+  if (data == 0) {
+    digitalWrite(led, HIGH); // Turn on the LED
+    char turu[] = "Object detected"; // Message when an object is detected
+    Serial.println(turu); // Print the message to the serial monitor
+  } else {
+    digitalWrite(led, LOW); // Turn off the LED
+    char furu[] = "Object not detected"; // Message when no object is detected
+    Serial.println(furu); // Print the message to the serial monitor
+  }
+
+  delay(100); // Delay for stability and to avoid rapid sensor readings
 }

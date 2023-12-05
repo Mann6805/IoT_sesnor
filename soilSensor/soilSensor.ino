26ffe274dc09e376dcff1fd627a1ac1c0ca703ec
Mann Patel <115395169+Mann6805@users.noreply.github.com>
2023-12-05 22:20:14 +0530

    Update soilSensor.ino

--- soilSensor/soilSensor.ino
+++ soilSensor/soilSensor.ino
@@ -1,14 +1,12 @@
-const int soilSensor = 36;// a0 wala 
+const int soilSensor = 36; // Define the pin (A0) where the soil moisture sensor is connected
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(soilSensor, INPUT);
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  pinMode(soilSensor, INPUT); // Set the soil moisture sensor pin as input
 }
 
-void loop()
-{
-  int data = digitalRead(soilSensor);
-  Serial.println(data);
-  delay(1000);  
+void loop() {
+  int data = analogRead(soilSensor); // Read analog data from the soil moisture sensor
+  Serial.println(data); // Print the sensor reading to the serial monitor
+  delay(1000); // Delay for 1 second before taking the next reading
 }
